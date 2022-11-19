99f00bc75e2750d0b24889a26b0d605e1169d719
Angelcaki99 <angelnestoroski153@gmail.com>
2022-11-19 20:02:43 +0100

    Angel

--- vezbi 1 .1/yes.js
+++ vezbi 1 .1/yes.js
@@ -1,4 +1,16 @@
 console.log("Test");
 let length = 4;
 let lastName = "Cars";
+console.log("length:" + length);
+let a = "10";
+let b = "20";
+let c = a/b;
+console.log(c);
+
+console.log("the sum of"+ a + " and the sum of  " + b + " is: " + (a+b));
+
+
+
+
+
 
