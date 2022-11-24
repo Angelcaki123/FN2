bfebb1fd8853b0980576ac5c93c6fd8224f39bce
Angelcaki99 <angelnestoroski153@gmail.com>
2022-11-24 19:12:13 +0100

    Age

---
+++ java cas .3/ex1.2.3/yes.js
@@ -0,0 +1,13 @@
+prompt("Nekoj teskt");
+
+let a =prompt("Please enter the value for a");
+console.log("a");
+
+let b = prompt("Please enter the value for b");
+
+a = Number(a) * Number(a);
+b = b * b;
+
+c = a + b;
+c = Math.pow
+
